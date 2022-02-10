2e44071017c0b1e7b751a19aef9233841a324561
Carson Woods <cwoods289@gmail.com>
2022-02-09 22:31:54 -0500

    Improve tests through inclusion of assert statements

--- tests/tests.rs
+++ tests/tests.rs
@@ -40,12 +40,14 @@ fn test_id_from_data() {
 
 #[test]
 fn test_id_to_json() {
+    let id_str = r#"{"uuid":"6db53f85-e3a2-4c1f-9471-d2429019736d","first_name":"First","middle_name":"Middle","last_name":"Last","date_of_birth":"1970-01-01","emails":["example@example.com"],"certified_extensions":[{"US_SSN":{"ssn":"000000000"}}]}"#;
+
     // Test conversion of ID to json object
     let test_ssn = US_SSN::new(String::from("000000000")).unwrap();
 
     // test manual construction of rule
     let _test_id = Id {
-        uuid: Uuid::new_v4(),
+        uuid: Uuid::parse_str("6db53f85-e3a2-4c1f-9471-d2429019736d").unwrap(),
         first_name: String::from("First"),
         middle_name: String::from("Middle"),
         last_name: String::from("Last"),
@@ -57,6 +59,30 @@ fn test_id_to_json() {
     // Convert the ID to a JSON string.
     let serialized = _test_id.to_json();
 
+    assert_eq!(id_str, serialized);
+
+}
+
+#[test]
+fn test_json_to_id() {
+    // Test conversion of ID to json object
+    let test_ssn = US_SSN::new(String::from("000000000")).unwrap();
+
+    // test manual construction of rule
+    let _test_id = Id {
+        uuid: Uuid::parse_str("6db53f85-e3a2-4c1f-9471-d2429019736d").unwrap(),
+        first_name: String::from("First"),
+        middle_name: String::from("Middle"),
+        last_name: String::from("Last"),
+        date_of_birth: NaiveDate::from_ymd(1970, 01, 01),
+        emails: vec![String::from("example@example.com")],
+        certified_extensions: vec![CertifiedExtension::US_SSN(test_ssn)],
+    };
+
+    let id_str = r#"{"uuid":"6db53f85-e3a2-4c1f-9471-d2429019736d","first_name":"First","middle_name":"Middle","last_name":"Last","date_of_birth":"1970-01-01","emails":["example@example.com"],"certified_extensions":[{"US_SSN":{"ssn":"000000000"}}]}"#;
+
     // Must be run with `cargo test -- --nocapture`
-    println!("ID JSON = {}", serialized);
+    let deserialized: Id = serde_json::from_str(&id_str).unwrap();
+
+    assert_eq!(_test_id, deserialized);
 }
